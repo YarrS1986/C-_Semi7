4e55e456a796c286d205168f76c3513679acbd02
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-16 21:55:01 +0500

    Задача4. Нашли все элементы в массиве

---
+++ exapmle4_FindIn2DimArray/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив. Введите элемент, и найдите первое его вхождение, 
+// выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+// Например, такой массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Введенный элемент 2, результат: [1, 4]                    элемент 6, результат: такого элемента нет.
+
+Console.Clear();
+
+int m = Promt("Введи количество строк(m) = ");
+int n = Promt("Введи количество столбцов(n) = ");
+
+int[,] matrix = FillArray(m, n);
+PrintArray(matrix);
+
+Console.WriteLine();
+
+int value = Promt("Введи искомую цифру = ");
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (value == matrix[i, j])
+        {
+            Console.Write($"[{i + 1}, {j + 1}]");
+            break;      // Неработает прекращение выполнения
+        }
+    }
+}
+// Console.Write($"Такого элемента нет!");     -  Незнаю куда это вставить ? 
+
+//--------------------------------------------
+int Promt(string message)
+{
+    Console.Write(message);
+    string mess = Console.ReadLine();
+    int answer = int.Parse(mess);
+    return answer;
+}
+
+int[,] FillArray(int row, int collumns)
+{
+    int[,] array = new int[row, collumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
