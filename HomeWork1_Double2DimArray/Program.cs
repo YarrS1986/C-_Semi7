8f28a5a843bf0a540580ebc68062ec47a09778fb
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-16 22:32:50 +0500

    ДЗ1. Поиск вещественного числа

---
+++ HomeWork1_Double2DimArray/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// Пример m = 3, n = 4.
+// 0,5  7    -2   -0,2
+// 1   -3,3   8   -9,9
+// 8    7,8  -7,1  9
+
+Console.Clear();
+
+int m = Promt("Введи количество строк(m) = ");
+int n = Promt("Введи количество столбцов(n) = ");
+
+double[,] matrix = FillArray(m, n);
+PrintArray(matrix);
+
+
+//--------------------------------------------
+int Promt(string message)
+{
+    Console.Write(message);
+    string mess = Console.ReadLine();
+    int answer = int.Parse(mess);
+    return answer;
+}
+
+double[,] FillArray(int row, int collumns)
+{
+    double[,] array = new double[row, collumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * (10 - -10) - 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:0.00}\t ");
+        }
+        Console.WriteLine();
+    }
+}
