53ea26aa552879feb827330a127316f2c3cef76f
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-16 20:05:59 +0500

    Задача0. ЗАполнили двумерный массив

--- exapmle0_2DimensionalArray/Program.cs
+++ exapmle0_2DimensionalArray/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+Console.Clear();
+
+int[,] matrix = FillArray(5, 4);
+PrintArray(matrix);
+
+
+//--------------------------------------------
+
+int[,] FillArray(int row, int collumns)
+{
+    int[,] array = new int[row, collumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-10, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
